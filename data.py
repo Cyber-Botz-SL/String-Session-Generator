27566a33ab5fdba63e04928fc713b50424924247
DR STRANGER <102143931+DrStrangerXD@users.noreply.github.com>
2022-08-02 20:31:16 +0530

    Update data.py

--- data.py
+++ data.py
@@ -8,8 +8,8 @@ class Data:
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("⚙ sᴜᴩᴩᴏʀᴛ", url="https://t.me/AlphaTm_Botz"),
-         InlineKeyboardButton("👨‍💻 ᴅᴇᴠᴇʟᴏᴩᴇʀ", url="https://t.me/xMalitha"),
+        [InlineKeyboardButton("⚙ sᴜᴩᴩᴏʀᴛ", url="https://t.me/Cyber_Botz_SL"),
+         InlineKeyboardButton("👨‍💻 ᴅᴇᴠᴇʟᴏᴩᴇʀ", url="https://t.me/Dr_Stranger_XD"),
         ],
     ]
 
@@ -19,6 +19,6 @@ Hᴇʏ {},
 Tʜɪs ɪs {},
 Aɴ ᴏᴩᴇɴ sᴏᴜʀᴄᴇᴅ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
 
-Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://github.com/xMalitha/stringenbotX)
-Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [мαℓιтнα мιнιяαηgα 🇱🇰](https://t.me/xMalitha) !
+Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://github.com/Cyber-Botz-SL)
+Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [𝘿𝙍 𝙎𝙏𝙍𝘼𝙉𝙂𝙀𝙍🇱🇰](https://t.me/Dr_Stranger_XD) !
     """
